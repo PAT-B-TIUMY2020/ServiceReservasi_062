ccc00646a9d724bf1809da4ca67151d511291b5c
Annisa Dian Amarta <anisadiana525@gmail.com>
2020-11-27 16:25:01 +0700

    add service

--- ServiceReservasi/IService1.cs
+++ ServiceReservasi/IService1.cs
@@ -7,39 +7,80 @@ using System.Text;
 
 namespace ServiceReservasi
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    
     [ServiceContract]
     public interface IService1
     {
         [OperationContract]
-        string GetData(int value);
+        string pemesanan(string IDPemesanan, string NamaCustomer, string NoTelepon, int JumlahPemesanan, string IDLokasi); //method //proses input data
+
 
         [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
+        string editPesanan(string IDPemesanan, string NamaCustomer);
+
+        [OperationContract]
+        string deletePemesanan(string IDPemesanan);
+
+        [OperationContract]
+        List<CekLokasi> ReviewLokasi(); //menampilkan data yang ada di database (select all) // menampilkan isi dari yang ada contact
+
+        [OperationContract]
+        List<DetailLokasi> DetailLokasi(); //menampilkan detail lokasi
+
+        [OperationContract]
+        List<Pemesanan> Pemesanan();
 
         // TODO: Add your service operations here
     }
 
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "ServiceReservasi.ContractType".
+
     [DataContract]
-    public class CompositeType
+    public class CekLokasi // daftar lokasi nongrong
     {
-        bool boolValue = true;
-        string stringValue = "Hello ";
+        [DataMember]
+        public string IDLokasi { get; set; } //variabel dari public class
 
         [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
+        public string NamaLokasi { get; set; }
 
         [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
+        public string DeskripsiSingkat { get; set; }
+    }
+
+    [DataContract]
+    public class DetailLokasi // daftar lokasi nongrong
+    {
+        [DataMember]
+        public string IDLokasi { get; set; } //variabel dari public class
+
+        [DataMember]
+        public string NamaLokasi { get; set; }
+
+        [DataMember]
+        public string DeskripsiFull { get; set; }
+
+        [DataMember]
+        public int Kuota { get; set; }
+
+    }
+
+    [DataContract]
+    public class Pemesanan // create
+    {
+        [DataMember]
+        public string IDPemesanan { get; set; } //variabel dari public class
+
+        [DataMember]
+        public string NamaCustomer { get; set; }
+
+        [DataMember]
+        public string NoTelepon { get; set; }
+
+        [DataMember]
+        public int JumlahPemesanan { get; set; }
+
+        [DataMember]
+        public string IDLokasi { get; set; }
+
     }
 }
