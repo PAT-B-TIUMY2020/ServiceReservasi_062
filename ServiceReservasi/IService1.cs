e99dafe4c65ad5f4ca0bbf6535ebc24cb379f89b
Annisa Dian Amarta <anisadiana525@gmail.com>
2020-12-13 18:14:39 +0700

    Login dan register

--- ServiceReservasi/IService1.cs
+++ ServiceReservasi/IService1.cs
@@ -30,6 +30,22 @@ namespace ServiceReservasi
         [OperationContract]
         List<Pemesanan> Pemesanan();
 
+        [OperationContract]
+        string Login(string username, string password);
+
+        [OperationContract]
+        string Register(string username, string password, string kategori);
+
+        [OperationContract]
+        string UpdateRegister(string username, string password, string kategori, int id);
+
+        [OperationContract]
+        string DeleteRegister(string username);
+
+        [OperationContract]
+        List<DataRegister> DataRegist();
+
+
         // TODO: Add your service operations here
     }
 
@@ -82,5 +98,21 @@ namespace ServiceReservasi
         [DataMember]
         public string Lokasi { get; set; }
 
+    }
+
+    [DataContract]
+    public class DataRegister
+    {
+        [DataMember(Order = 1)]
+        public int id { get; set; }
+
+        [DataMember(Order = 2)]
+        public string username { get; set; }
+
+        [DataMember(Order = 3)]
+        public string password { get; set; }
+
+        [DataMember(Order = 4)]
+        public string kategori { get; set; }
     }
 }
